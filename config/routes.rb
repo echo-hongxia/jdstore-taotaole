0692a4634e5ab254751ea82c5f96dda11116bc3e
apple <apple@appledeMacBook-Pro.local>
2017-02-21 11:26:04 +0800

    implement backend product  CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
+  namespace :admin do
+    resources :products
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
+  end
+  
 end
