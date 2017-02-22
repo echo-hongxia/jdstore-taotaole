0da218a12114c0f0453001014ab7a0eb3f7d2fbf
apple <apple@appledeMacBook-Pro.local>
2017-02-22 20:35:14 +0800

    add uploader Avatar

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -14,22 +14,39 @@ class Admin::ProductsController < ApplicationController
 
   def update
     @product = Product.find(params[:id])
+    if params[:photos] != nil
+       @product.photos.destroy_all #need to destroy old pics first
 
-    if @product.update(product_params)
-      redirect_to admin_products_path
-    else
-      render :edit
-    end
+       params[:photos]['avatar'].each do |a|
+         @picture = @product.photos.create(:avatar => a)
+       end
+
+       @product.update(product_params)
+       redirect_to admin_products_path
+
+     elsif @product.update(product_params)
+       redirect_to admin_products_path
+     else
+       render :edit
+     end
   end
+
+
 
   def new
      @product = Product.new
+     @photo = @product.photos.build # for create multi-photos
   end
 
    def create
      @product = Product.new(product_params)
 
      if @product.save
+       if params[:photos] != nil
+             params[:photos]['avatar'].each do |a|
+             @photo = @product.photos.create(:avatar => a)
+         end
+       end 
        redirect_to admin_products_path
      else
        render :new
