47eee2c52a30b417a42ea895b37dbf7394397e5a
apple <apple@appledeMacBook-Pro.local>
2017-02-22 21:35:36 +0800

    add ratyrate gem

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :photos
   accepts_nested_attributes_for :photos
+  ratyrate_rateable "speed", "engine", "price"
+  has_many :prints  #产品详情图
+  accepts_nested_attributes_for :prints
 end
