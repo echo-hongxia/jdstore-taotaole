6fa5b00a644ecf93ea21b046c75f222657482f59
apple <apple@appledeMacBook-Pro.local>
2017-02-24 21:46:45 +0800

    adjust goodsdetail css html

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,3 +29,21 @@ $(document).on('mouseover', '.intro-preview-item', function () {
   var src = $(this).find('img').attr('src') //从被鼠标选中的图片的src里面拿到图片链接
   $('.gods-main-pic').find('img').attr('src', src) //把图片链接设置到大图的src里面
 })
+
+// 菜单栏切换
+$(document).on('click', '.nav-item', function () {
+  var index = $(this).index() //拿到这个「选项卡」的index，第一个是0，第二个是1，以此类推
+  $('.test-item').hide() //所有的内容都隐藏
+  $('.test-item').eq(index).show() //只显示对于index的内容
+})
+
+// 放大镜
+$('#preview').css('visibility', 'hidden')
+var evt = new Event(),
+    m = new Magnifier(evt);
+m.attach({
+    thumb: '#thumb',
+    large: $('.intro-preview-activeItem img').attr('src'),
+    largeWrapper: 'preview',
+    zoom: 2
+})
